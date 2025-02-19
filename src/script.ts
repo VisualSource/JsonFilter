d826c799d59fe58e5b04d3c4ddfa75059e261abc
VisualSource <collin_blosser@yahoo.com>
2025-02-19 00:26:27 -0500

    add multi document support

--- src/script.ts
+++ src/script.ts
@@ -3,15 +3,24 @@ import type JSONEditor from "jsoneditor";
 type Filter = {
     id: string;
     editor?: AceAjax.Editor;
-    type: "filter" | "select" | "map";
+    type: "filter" | "select" | "map" | "flatmap";
     func: (...args: unknown[]) => unknown
 }
 
 type ElementProps = {
+    textContent?: string;
+    type?: string;
+    name?: string;
+    placeholder?: string;
+    required?: boolean;
+    minlength?: number;
+    readonly?: boolean;
     class?: string;
     id?: string;
     value?: string;
+    [dataAttr: `data-${string}`]: string;
     selected?: boolean;
+    onClick?: (ev: Event) => void
 }
 
 function debounce(func: (...args: unknown[]) => unknown, timeout = 300) {
@@ -26,13 +35,21 @@ function debounce(func: (...args: unknown[]) => unknown, timeout = 300) {
 function createElement<K extends keyof HTMLElementTagNameMap>(tag: K, attrs?: ElementProps) {
     const el = document.createElement(tag);
 
-    if (attrs) for (const [key, value] of Object.entries(attrs)) {
-        if (typeof value === "boolean") {
-            if (value) el.setAttribute(key, "");
-            continue;
+    if (attrs) {
+        const { textContent, onClick, ...props } = attrs;
+        if (textContent) el.textContent = textContent;
+        if (onClick) el.addEventListener("click", onClick);
+        for (const [key, value] of Object.entries(props)) {
+            if (typeof value === "boolean") {
+                if (value) el.setAttribute(key, "");
+                continue;
+            } else if (typeof value === "number") {
+                el.setAttribute(key, value.toString());
+                continue;
+            } else if (value) {
+                el.setAttribute(key, value);
+            }
         }
-
-        el.setAttribute(key, value);
     }
 
     return el;
@@ -50,6 +67,13 @@ function createSelectElement(options: { label: string; value: string, selected?:
     return el;
 }
 
+/**
+ * Gets a element by id, throws if it can be found.
+ *
+ * @template T
+ * @param {string} target
+ * @return {*}  {T}
+ */
 function getElement<T = HTMLElement>(target: string): T {
     const el = document.getElementById(target);
     if (!el) throw new Error(`Failed to get element with id of "${target}"`);
@@ -85,8 +109,8 @@ class App {
     }
 
     private editor: JSONEditor;
-    private source?: string;
-    private content?: unknown;
+    private sources: { id: string; url: string }[] = [];
+    private content: { id: string; data: unknown }[] = [];
     private filtersGroups: Filter[][] = [];
     private activeFilterGroup: number = 0;
 
@@ -99,8 +123,61 @@ class App {
 
     private init() {
         // load content
-        const btnLoadJson = getElement<HTMLButtonElement>("btn-load-json");
-        btnLoadJson.addEventListener("click", this.loadJson);
+        const dialogEdit = getElement<HTMLDialogElement>("source-input-url");
+        const btnAddSource = getElement<HTMLButtonElement>("add-source");
+        btnAddSource.addEventListener("click", () => {
+            dialogEdit.showModal();
+        });
+
+        const btnCancel = getElement<HTMLButtonElement>("dialog-add-btn-cancel");
+        btnCancel.addEventListener("click", () => {
+            dialogEdit.close();
+        });
+
+        const dialogForm = getElement<HTMLFormElement>("dialog-form");
+        dialogForm.addEventListener("submit", (ev) => {
+            const content = new FormData(ev.target as HTMLFormElement);
+            const source = content.get("source")?.toString().trim();
+            if (!source) return;
+
+            const contentId = crypto.randomUUID();
+            this.sources.push({ url: source, id: contentId });
+            this.createSourcesList();
+
+            fetch(source).then(e => e.json()).then(item => {
+                this.content.push({ id: contentId, data: item });
+                this.process();
+                this.save();
+            }).catch((error) => {
+                alert("Failed to load source")
+                console.error(error)
+            });
+        });
+
+        const dialogEdiForm = getElement<HTMLFormElement>("dialog-edit-form");
+        dialogEdiForm.addEventListener("submit", (ev) => {
+            const content = new FormData(ev.target as HTMLFormElement);
+            const url = content.get("source")?.toString().trim();
+            const id = content.get("id")?.toString();
+            if (!id || !url) return;
+
+            const idx = this.sources.findIndex(e => e.id === id);
+            if (idx === -1) return;
+
+            this.sources[idx].url = url;
+
+            fetch(url).then(e => e.json()).then((item) => {
+                const contentIdx = this.content.findIndex(e => e.id === id);
+                if (contentIdx === -1) return;
+                this.content[contentIdx].data = item;
+
+                this.process();
+                this.save();
+            }).catch(e => {
+                console.error(e);
+                alert("Failed to load source");
+            });
+        });
 
         // filter control
         const btnFilterReset = getElement<HTMLButtonElement>("btn-filter-reset");
@@ -136,7 +213,7 @@ class App {
         for (const filter of filters) {
             const root = createElement("li", { class: "filter-item" })
 
-            const select = createSelectElement(["filter", "select", "map"].map(e => ({
+            const select = createSelectElement(["filter", "select", "map", "flatmap"].map(e => ({
                 label: e.replace(/^\w/, e[0].toUpperCase()),
                 value: e,
                 selected: filter.type === e
@@ -212,8 +289,8 @@ class App {
     }
 
     private process() {
-        let data = this.content;
-        if (!data) return;
+        let data: unknown[] = this.content.map(e => e.data);
+        console.log(data);
 
         const filters = this.filtersGroups.at(this.activeFilterGroup);
 
@@ -222,6 +299,10 @@ class App {
                 case "filter":
                     if (!Array.isArray(data)) break;
                     data = data.filter(filter.func);
+                    break;
+                case "flatmap":
+                    if (!Array.isArray(data)) break;
+                    data = data.flatMap(filter.func);
                     break;
                 case "select":
                     if (!data || typeof data !== "object") return;
@@ -238,22 +319,6 @@ class App {
         }
 
         this.editor.set(data);
-    }
-
-    private loadJson = async () => {
-        const input = getElement<HTMLInputElement>("content-url");
-        const url = input.value.trim();
-
-        try {
-            const response = await fetch(url);
-            this.content = await response.json();
-            this.source = url;
-            this.process();
-            this.save();
-        } catch (error) {
-            console.error(error);
-            alert("There was an error loading the json file");
-        }
     }
     private selectFilterGroupChange = (ev: Event) => {
         try {
@@ -324,27 +389,95 @@ class App {
     }
     private save() {
         localStorage.setItem("jfs", JSON.stringify({
-            source: this.source,
+            sources: this.sources,
             filters: this.filtersGroups,
             active: this.activeFilterGroup
         }, sterilizeFunction));
     }
+
+    private createSourcesList() {
+        const list = getElement("source-list");
+
+        for (let index = 0; index < list.children.length; index++) {
+            list.children.item(index)?.remove();
+        }
+
+        for (const source of this.sources) {
+            const el = createElement("li", { class: "source-li", "data-id": source.id });
+            el.appendChild(createElement("input", {
+                class: "input-cl",
+                type: "url",
+                readonly: true,
+                value: source.url.split("/").at(-1),
+                placeholder: "https://urltofile.json"
+            }));
+            el.appendChild(createElement("button", {
+                type: "button",
+                textContent: "Edit",
+                class: "btn",
+                onClick: (ev) => {
+                    const parent = (ev.target as HTMLLIElement).parentElement;
+                    const id = parent?.getAttribute("data-id");
+                    if (!id) return;
+                    const core = this.sources.find(e => e.id === id);
+                    if (!core) return;
+
+                    const dialog = getElement<HTMLDialogElement>("source-edit-dialog");
+
+                    const inputs = dialog.querySelectorAll("input")
+                    const idInput = inputs.item(0);
+                    const urlInput = inputs.item(1);
+                    idInput.value = core.id;
+                    urlInput.value = core.url;
+                    dialog.showModal();
+                },
+            }));
+            el.appendChild(createElement("button", {
+                type: "button",
+                textContent: "X",
+                class: "btn btn-error",
+                onClick: (ev) => {
+                    const parent = (ev.target as HTMLLIElement).parentElement;
+                    const id = parent?.getAttribute("data-id");
+                    if (!id) return;
+                    const idx = this.sources.findIndex(e => e.id === id);
+                    if (idx === -1) return;
+                    if (parent) parent.remove();
+                    this.sources.splice(idx, 1);
+                    this.content.splice(idx, 1);
+                    this.process();
+                    this.save();
+                },
+            }));
+            list.appendChild(el);
+
+
+        }
+    }
+
     private async load() {
         const state = localStorage.getItem("jfs");
         if (!state) return;
 
-        const data = JSON.parse(state, desterilizeFunction) as { source?: string; filters: Filter[][], active: number; };
+        const data = JSON.parse(state, desterilizeFunction) as { sources: { id: string; url: string }[]; filters: Filter[][], active: number; };
 
         this.filtersGroups = data.filters;
         this.activeFilterGroup = data.active;
 
-        this.source = data.source;
-
-        if (this.source) {
-            const el = getElement<HTMLInputElement>("content-url");
-            el.value = this.source;
-            await this.loadJson();
+        this.sources = data.sources;
+        if (this.sources.length) {
+            this.createSourcesList();
         }
+
+        const requests = this.sources.map(e => fetch(e.url).then(d => d.json()).then((item) => ({ data: item, id: e.id })));
+
+        await Promise.all(requests).then((items) => {
+            this.content = items;
+            this.process();
+        }).catch(e => {
+            console.error(e);
+            alert("Failed to load sources")
+        });
     }
 }
 
